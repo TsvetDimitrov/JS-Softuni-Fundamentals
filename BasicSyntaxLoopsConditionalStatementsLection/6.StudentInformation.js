9fe3e9ca4112df717ce360c80efb7b7cb13efd46
Wyasabi <71334626+Wyasabi@users.noreply.github.com>
2021-01-13 21:33:17 +0200

    Add files via upload

---
+++ BasicSyntaxLoopsConditionalStatementsLection/6.StudentInformation.js
@@ -0,0 +1,9 @@
+// You will be given 3 parameters – student name (string), age (number) and average grade (number). 
+// Your task is to print all the info about the student in the following format. 
+// The grade should be formatted to the second decimal point: "Name: {student name}, Age: {student age}, Grade: {student grade}".
+function solve (name, age, grade){
+
+console.log(`Name: ${name}, Age: ${age}, Grade: ${(grade).toFixed(2)}`);
+}
+
+solve('John', 15, 5.54678);
