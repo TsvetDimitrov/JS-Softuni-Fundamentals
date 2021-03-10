e41a1edbb34a03b7f64b52f97290a1c14d8a5eab
Tsvetomir <dimitrovv32@gmail.com>
2021-03-10 21:19:51 +0200

    Add files via upload

---
+++ TextProcessingLab/1.PrintCharacters.js
@@ -0,0 +1,11 @@
+// Write a function that receives a string and prints all the characters on separate lines.
+
+
+function printCharacters(word){
+    for (let char of word) {
+        console.log(char);
+    }
+
+}
+
+printCharacters('AWord');
