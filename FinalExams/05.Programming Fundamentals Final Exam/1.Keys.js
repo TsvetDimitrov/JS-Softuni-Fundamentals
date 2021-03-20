361e58385c42e1b32a582a80dd0fa8163629a21e
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-03-20 18:53:45 +0200

    Edited in paths. *TESTING

---
+++ FinalExams/05.Programming Fundamentals Final Exam/1.Keys.js
@@ -0,0 +1,98 @@
+// You are about to make some good money, but first you need to think of a way to 
+// verify who paid for your product and who didn`t. You have decided to let people use the software 
+// for a free trial period and then require an activation key in order to continue to use the product. 
+// The last step before you could cash out is to design a program that creates unique activation keys for each user. 
+// So, waste no more time and start typing!
+
+// The first line of the input will be your raw activation key. It will consist of letters and numbers only. 
+// After that, until the "Generate" command is given, you will be receiving strings with instructions for different 
+// operations that need to be performed upon the raw activation key.
+// There are several types of instructions, split by ">>>":
+// •	Contains>>>{substring} – checks if the raw activation key contains the given substring.
+// o	If it does prints: "{raw activation key} contains {substring}".
+// o	If not, prints: "Substring not found!"
+// •	Flip>>>Upper/Lower>>>{startIndex}>>>{endIndex}
+// o	Changes the substring between the given indices (the end index is exclusive) to upper or lower case. 
+// o	All given indexes will be valid.
+// o	Prints the activation key.
+// •	Slice>>>{startIndex}>>>{endIndex}
+// o	Deletes the characters between the start and end indices (end index is exclusive).
+// o	Both indices will be valid.
+// o	Prints the activation key.
+// Input
+// •	The first line of the input will be string and it will consist of letters and numbers only. 
+// •	After the first line, until the "Generate" command is given, you will be receiving strings.
+// Output
+// •	After the "Generate" command is received, print:
+// o	"Your activation key is: {activation key}"
+
+
+function keys(input) {
+
+
+    let actions = {
+        Contains(key, string) {
+            if (key.includes(string)) {
+                console.log(`${key} contains ${string}`);
+            } else {
+                console.log(`Substring not found!`);
+            }
+            return key;
+
+        },
+
+        Flip(key, mode, startIndex, endIndex) {
+            [startIndex, endIndex] = [Number(startIndex), Number(endIndex)];
+            let firstPart = key.substring(0, startIndex);
+            let secondPart = key.substring(startIndex, endIndex);
+            let thirdPart = key.substring(endIndex);
+            if (mode === "Lower") {
+
+                secondPart = secondPart.toLocaleLowerCase();
+            } else {
+                secondPart = secondPart.toLocaleUpperCase();
+            }
+            let result = firstPart + secondPart + thirdPart;
+
+            console.log(result);
+            return result;
+        },
+
+        Slice(key, startIndex, endIndex) {
+            [startIndex, endIndex] = [Number(startIndex), Number(endIndex)];
+            let firstPart = key.substring(0, startIndex);
+            let secondPart = key.substring(startIndex, endIndex);
+            let thirdPart = key.substring(endIndex);
+
+            let result = firstPart + thirdPart;
+
+            console.log(result);
+            return result;
+
+        }
+
+    }
+    let key = input.shift();
+    let line;
+
+    while ((line = input.shift()) !== "Generate") {
+        let [actionName, ...args] = line.split(">>>");
+
+        key = actions[actionName](key, ...args);
+    }
+
+    console.log(`Your activation key is: ` + key);
+}
+
+
+
+
+keys([
+    'abcdefghijklmnopqrstuvwxyz',
+    'Slice>>>2>>>6',
+    'Flip>>>Upper>>>3>>>14',
+    'Flip>>>Lower>>>5>>>7',
+    'Contains>>>def',
+    'Contains>>>deF',
+    'Generate'
+])
---
+++ FinalExams/05.Programming Fundamentals Final Exam/3.P!rates.js
@@ -0,0 +1,223 @@
+// Anno 1681. The Caribbean. The golden age of piracy. You are a well-known pirate captain by the name of Jack… Daniels. 
+// Together with your comrades Jim (Beam) and Johnny (Walker) you have been roaming the seas, 
+// looking for gold and treasure… and the occasional killing, of course. Go ahead, target some wealthy settlements and show 
+// them the pirate`s way!
+// Description
+// Until the "Sail" command is given you will be receiving:
+// •	Cities that you and your crew have targeted, with their population and gold, separated by "||".
+// •	If you receive a city which has been already received, you have to increase the population and gold with the given values.
+// After the "Sail" command, you will start receiving lines of text representing events until the "End" command is given. 
+// Events will be in the following format:
+// •	"Plunder=>{town}=>{people}=>{gold}"
+// o	You have successfully attacked and plundered the town, killing the given number of 
+// people and stealing the respective amount of gold. 
+// o	For every town you attack print this message: "{town} plundered! {gold} gold stolen, {people} citizens killed."
+// o	If any of those two values (population or gold) reaches zero, the town is disbanded.
+// 	You need to remove it from your collection of targeted cities and print the following message:
+//  "{town} has been wiped off the map!"
+// o	There will be no case of receiving more people or gold than there is in the city.
+// •	"Prosper=>{town}=>{gold}"
+// o	There has been a dramatic economic growth in the given city, increasing its treasury by the given amount of gold.
+// o	The gold amount can be a negative number, so be careful. If a negative amount of gold is given print:
+//  "Gold added cannot be a negative number!" and ignore the command.
+// o	If the given gold is a valid amount, increase the town's gold reserves by the respective amount and print the following
+//  message: "{gold added} gold added to the city treasury. {town} now has {total gold} gold."
+// Input
+// •	On the first lines, until the "Sail" command, you will be receiving strings representing the 
+// cities with their gold and population, separated by "||"
+// •	On the next lines, until the "End" command, you will be receiving strings representing the actions described above, 
+// separated by "=>"
+// Output
+// •	After receiving the "End" command if there are any existing settlements on your list of targets, you need to print 
+// all of them, sorted by their gold in descending order, then by their name in ascending order, in the following format:
+// Ahoy, Captain! There are {count} wealthy settlements to go to:
+// {town1} -> Population: {people} citizens, Gold: {gold} kg
+//    …
+// {town…n} -> Population: {people} citizens, Gold: {gold} kg
+// •	If there are no settlements left to plunder, print:
+// "Ahoy, Captain! All targets have been plundered and destroyed!"
+// Constraints
+// •	The initial population and gold of the settlements will be valid, 32-bit integers, 
+// will never be negative or exceed the respective limits.
+// •	The town names in the events will always be valid towns that should be on your list.
+
+
+
+
+
+
+
+function pirates(input) {
+    let actions = {
+        Plunder(towns, townName, people, gold) {
+            let town = towns[townName];
+            town.population -= Number(people);
+            town.gold -= Number(gold);
+        
+            console.log(`${townName} plundered! ${gold} gold stolen, ${people} citizens killed.`);
+        
+            if (town.population <= 0 || town.gold <= 0) {
+                console.log(`${townName} has been wiped off the map!`);
+                delete towns[townName];
+            }
+        },
+        
+        Prosper(towns, townName, gold) {
+        
+            let town = towns[townName];
+            if(Number(gold) > 0 ){
+                town.gold += Number(gold);
+                console.log(`${gold} gold added to the city treasury. ${townName} now has ${town.gold} gold.`);
+            }else{
+                console.log(`Gold added cannot be a negative number!`);
+            } 
+        }
+    }
+    let towns = {}
+    let line;
+
+
+
+    while((line = input.shift()) != "Sail"){
+        let [townName, population, gold] = line.split("||");
+
+        if(!towns.hasOwnProperty(townName)){
+            towns[townName] = {population: 0, gold: 0};
+        }
+
+        towns[townName].population += Number(population);
+        towns[townName].gold += Number(gold);
+    }
+
+    while((line = input.shift())!= "End"){
+        let [actionName, townName, ...args] = line.split("=>");
+        
+
+        let action = actions[actionName];
+        action(towns, townName, ...args);
+
+    }
+
+    let sorted = Object.entries(towns).sort(compareTowns);
+
+    if(sorted.length > 0){
+        console.log(`Ahoy, Captain! There are ${sorted.length} wealthy settlements to go to:`);
+
+        for (let town of sorted) {
+            console.log(`${town[0]} -> Population: ${town[1].population} citizens, Gold: ${town[1].gold} kg`)
+        }
+    }else{
+        console.log(`Ahoy, Captain! All targets have been plundered and destroyed!`);
+    }
+
+    function compareTowns([nameA, townA], [nameB, townB]){
+        return townB.gold - townA.gold || nameA.localeCompare(nameB);
+    }
+}
+
+pirates([
+    'Nassau||95000||1000',
+    'San Juan||930000||1250',
+    'Campeche||270000||690',
+    'Port Royal||320000||1000',
+    'Port Royal||100000||2000',
+    'Sail',
+    'Prosper=>Port Royal=>-200',
+    'Plunder=>Nassau=>94000=>750',
+    'Plunder=>Nassau=>1000=>150',
+    'Plunder=>Campeche=>150000=>690',
+    'End'
+  ]);
+
+
+
+  //another solution:
+
+
+
+
+  function piratesTEST(input) {
+    let citiesMap = {}
+
+
+    while (input[0] !== "Sail") {
+        let [cityName, people, gold] = input.shift().split("||");
+        people = Number(people);
+        gold = Number(gold);
+
+
+        if (citiesMap.hasOwnProperty(cityName)) {
+            citiesMap[cityName].people += people;
+            citiesMap[cityName].gold += gold;
+        } else {
+            citiesMap[cityName] = { gold, people }
+        }
+    }
+    input.shift();
+
+
+    while (input[0] !== "End") {
+        let [command, ...args] = input.shift().split("=>");
+
+
+        if (command === "Plunder") {
+            let [cityName, people, gold] = args;
+            people = Number(people);
+            gold = Number(gold);
+
+
+            citiesMap[cityName].people -= people;
+            citiesMap[cityName].gold -= gold;
+            console.log(`${cityName} plundered! ${gold} gold stolen, ${people} citizens killed.`);
+
+            if (citiesMap[cityName].people <= 0 || citiesMap[cityName].gold <= 0) {
+                console.log(`${cityName} has been wiped off the map!`);
+
+                delete citiesMap[cityName];
+            }
+        } else {
+            let [cityName, gold] = args;
+
+            gold = Number(gold);
+            if (gold < 0) {
+                console.log(`Gold added cannot be a negative number!`);
+            } else {
+                citiesMap[cityName].gold += gold;
+                console.log(`${gold} gold added to the city treasury. ${cityName} now has ${citiesMap[cityName].gold} gold.`);
+            }
+        }
+    }
+
+
+    let sortedCities = Object.entries(citiesMap).sort(sortCities);
+
+    if(sortedCities.length === 0){
+        console.log("Ahoy, Captain! All targets have been plundered and destroyed!");
+    }else{
+        console.log(`Ahoy, Captain! There are ${sortedCities.length} wealthy settlements to go to:`);
+
+
+        for (const [cityName, cityInfo] of sortedCities) {
+            let { gold, people } = cityInfo;
+
+            console.log(`${cityName} -> Population: ${people} citizens, Gold: ${gold} kg`)
+        }
+    }
+    
+
+
+    function sortCities(a, b){
+        let [aCityName, aCityInfo] = a;
+        let [bCityName, bCityInfo] = b;
+
+
+        let result = bCityInfo.gold - aCityInfo.gold;
+
+
+        if(result === 0){
+            return aCityName.localeCompare(bCityName);
+        }
+
+        return result;
+    }
+}
