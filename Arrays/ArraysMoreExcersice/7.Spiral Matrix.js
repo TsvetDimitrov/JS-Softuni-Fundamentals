29f53430eb496a6f983d76c6c6f8d234e3749737
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-04-06 14:11:16 +0300

    added solution.

---
+++ Arrays/ArraysMoreExcersice/7.Spiral Matrix.js
@@ -0,0 +1,12 @@
+// Write a function that generates a Spirally filled matrix with numbers, with given dimensions.
+// Input
+// The input comes as 2 numbers that represent the dimension of the matrix. 
+// Output
+// The output is the matrix filled spirally starting from 1. You need to print every row on a new line,
+//  with the cells separated by a space. Check the examples below. 
+
+
+function spiralMatrix(){
+
+}
+
